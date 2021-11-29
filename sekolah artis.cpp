f9c31fa621af5445704e5925739f64e1c2cda6d5
Egy Vedriyanto <90687836+egyvedriyanto78@users.noreply.github.com>
2021-11-29 10:27:36 +0700

    Add files via upload

---
+++ sekolah artis.cpp
@@ -0,0 +1,18 @@
+//Program ini dibuat oleh Egy vedriyanto pada responsi DDP menggunakan hacker rank
+
+//Sekolah Artis merupakan sebuah program untuk menentukan lulus tidaknya seorang pelajar dalam masuk sekolah unggulan
+
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+    int t, b, u;
+    cin>>t>>b>>u;
+    if(t>170 && b>55 && u>=15){cout<<"Lolos";}
+    else {cout<<"Tidak Lolos";}
+    return 0;
+}
